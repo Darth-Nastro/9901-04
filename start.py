3cf87739dbbd0f7d75602698b88962e0060ceed6
Darth-Nastro <mavericklast5@gmail.com>
2022-10-20 16:13:57 -0400

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,6 @@
-num = input()
+//itteration 1 this version only displays the values after input in the code under num
+// This also displays each value after each operation
+num = 1
 while num  != 1:
     if (num % 2) == 0 and num != 1:
       num = num // 2
@@ -6,3 +8,17 @@ while num  != 1:
     else:
          num =((num*3) + 1)
          print(num)
+// this section is the new code possibly looping. attempted test on repl.it but it didn' work due to cpu limit being reached.
+// will attempt at home on visual studio system specs-> Rog Strix amd rx 600
+num = 3 ** 3
+while True:
+  while num  != 1:
+      if (num % 2) == 0 and num != 1:
+        num = num // 2
+        print(num)
+      else:
+           num =((num*3) + 1)
+           print(num)
+if (num == 1):
+  num = num +1
+  print(num)
